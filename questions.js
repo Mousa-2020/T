df74c2e21139500ce4c16167b1bc109c6d354d55
Mousa  Al-bher <mousasaleh2024@gmail.com>
2026-06-26 10:11:53 +0300

    Create questions.js with sample MCQ
    Added a questions array with a sample multiple-choice question in English and Arabic.

---
+++ questions.js
@@ -0,0 +1,30 @@
+const questions = [
+
+{
+id:1,
+
+type:"mcq",
+
+question_en:
+"Who argued that Emotional Intelligence (EQ) is more critical for success than IQ?",
+
+question_ar:
+"من الذي جادل بأن الذكاء العاطفي (EQ) أكثر أهمية للنجاح من معامل الذكاء (IQ)؟",
+
+answers:[
+
+"Sigmund Freud",
+
+"Daniel Goleman",
+
+"Jean Piaget",
+
+"Lev Vygotsky"
+
+],
+
+correct:1
+
+},
+
+];
